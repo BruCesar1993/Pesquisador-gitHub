53ca5d40ee6b1a8631273b4ac5c5ccfb51d25d15
Bruno César Piovezan Mendonça <bcm_1993@hotmail.com>
2020-10-05 09:26:05 -0300

    atualizaçao

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
-import {Title} from './style'
+import {Title, Form, Repositories} from './style'
 import logo from '../../images/logo.svg'
 
 const dashboard: React.FC = ()=>{
-    return <Title>Pesquisador GitHub</Title>
+
+  return (
+
+    <>
+
+       <img src= {logo} alt= "GitHub Explorer"/>
+
+       <Title> Pesquisador GitHub </Title>
+
+       <Form>
+         <input placeholder= "Digite o nome do repositorio"/>
+         <button type= "submit">Pesquisar</button>
+
+       </Form>
+
+       <Repositories>
+         <a href= "teste"
+
+       </Repositories>
+
+    </>
+
+    )
+
 }
 
 export default dashboard
--- src/pages/dashboard/style.ts
+++ src/pages/dashboard/style.ts
@@ -1,7 +1,49 @@
 import styled from 'styled-components'
+import {shade} from 'polished'
 
 
  export const Title = styled.h1`
   font-size: 48px;
-  color: #3a3a3a
+  color: #3a3a3a;
+  margin-top: 70px;
+  max-width: 450px;
+  line-height: 36px;
+
+
 `
+
+
+ export const Form =styled.form `
+
+
+  margin-top : 40px;
+  max-width: 70px;
+
+  display: flex;
+
+  input{
+
+
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+    border: 1;
+    border-radius: 5px 0 0 5px
+
+
+
+  }
+
+
+  button {
+    width: 210px;
+    height: 70px;
+    background: #04D361;
+    border-radius: 0px 5px 5px 0px
+  }
+
+
+
+
+
+ `
